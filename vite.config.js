14ed02e4e6e7e182b213a8d5435fc2ac1aff8ab5
willianls88 <williansdev@gmail.com>
2025-11-02 20:30:23 -0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  root: "src/client", // pasta do frontend
+  build: {
+    outDir: "../../dist/client", // build sairá em /dist/client
+    emptyOutDir: true,
+  },
+  server: {
+    host: true,
+    port: 5173,
+  },
+});
